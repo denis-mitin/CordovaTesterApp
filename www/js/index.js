8950aaae9e8cedf6a8a6663e917203bf757f01a1
denis.mi <denis.mi@gigya-inc.com>
2016-09-05 16:28:44 +0300

    Tester updated

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var app;
+app = {
     // Application Constructor
     initialize: function () {
         this.bindEvents();
@@ -32,33 +33,123 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
+
+
+    callMethod: function () {
+        //console.log('in Call method');
+        //alert('in Call method');
+        var textLog = document.getElementById("txtres");
+
+        //document.getElementById("txtres").value = "Call method pressed\n";
+
+        var method = document.getElementById("method").value;
+        var params = eval("(" + document.getElementById("params").value + ")");
+
+        document.getElementById("txtres").value += "Calling " + method + " with params " + document.getElementById("params").value + "\n";
+
+        if (method == "login") {
+            var prov = params.provider;
+            delete params.provider;
+
+            gigyaClient.loginToProvider(prov, params)
+                .then(function(response) {
+                    document.getElementById("txtres").value += "Success \n" + JSON.stringify(response.data);
+                })
+                .catch(function(error) {
+                    document.getElementById("txtres").value += "Error \n" + JSON.stringify(error.data);
+                });
+        }
+        else if (method == "logout") {
+            gigyaClient.logout();
+        }
+        else if (method == "accounts.screenSet" || method == "gm.notifications" || method.indexOf("UI") != -1) {
+            gigyaClient.showPlugin(method, params, {
+                onLoad: function (event) {
+                    document.getElementById("txtres").value += "onLoad event \n";
+                    //document.getElementById("txtres").value += (JSON.stringify(event.detail));
+                },
+                onError: function (event) {
+                    document.getElementById("txtres").value += "onError event \n";
+                    document.getElementById("txtres").value += (JSON.stringify(event.detail));
+                },
+                onAfterSubmit: function (event) {
+                    document.getElementById("txtres").value += "AfterSubmit event \n";
+                    document.getElementById("txtres").value += (JSON.stringify(event.detail));
+                },
+                onDismiss: function (event) {
+                    document.getElementById("txtres").value += "Dissmiss event \n";
+                    //document.getElementById("txtres").value += (JSON.stringify(event.detail));
+                }
+            });
+
+        }
+
+        else {
+            gigyaClient.sendRequest(method, params, true)
+                .then(function(response) {
+                    document.getElementById("txtres").value += "Success \n" + JSON.stringify(response.data);
+                })
+                .catch(function(error) {
+                    document.getElementById("txtres").value += "Error \n" +JSON.stringify(error.data);
+                });
+        }
+
+    },
+
+    setSelect: function () {
+        //alert('in setSelect');
+        var paramsBox = document.getElementById("params");
+
+        switch (document.getElementById("method").value) {
+            case "login" :
+                paramsBox.value = "{\"provider\":\"facebook\"}";
+                break;
+            case "accounts.screenSet" :
+                paramsBox.value = "{\"screenSet\":\"mobile-RegistrationLogin\"}";
+                break;
+            case "comments.commentsUI" :
+                paramsBox.value = "{\"categoryID\":\"ios\"}";
+                break;
+            default :
+                paramsBox.value = "{}";
+        }
+    },
+
     onDeviceReady: function () {
-        app.receivedEvent('deviceready');
+        console.log('on device ready');
+
+        document.getElementById("callBtn").addEventListener("click", app.callMethod);
+
+        document.getElementById("method").addEventListener("change", app.setSelect);
+
+        document.getElementById("txtres").addEventListener('dblclick', function(){
+
+            document.getElementById("txtres").value="";
+
+        });
 
         gigyaClient.init('3_wJo2JBfQO09OzjVvvt6F-0oEd8igVPLLRRXXqgNGvOkQyVUP9wWZxg_kIEMnbE9n', 'us1.gigya.com');
 //        console.log(gigyaClient.getVersion());
-        gigyaClient.addEventListener(gigyaClient.Event.LOGIN, this.onLogin);
-        gigyaClient.addEventListener(gigyaClient.Event.LOGOUT, this.onLogout);
+        gigyaClient.addEventListener(gigyaClient.Event.LOGIN, app.onLogin);
+        gigyaClient.addEventListener(gigyaClient.Event.LOGOUT, app.onLogout);
 
-        gigyaClient.showPlugin('accounts.screenSet', {screenSet: 'Android-RegistrationLogin'});
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        app.setSelect();
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        //gigyaClient.showPlugin('accounts.screenSet', {screenSet: 'Android-RegistrationLogin'});
     },
     onLogin: function (event) {
-        alert(JSON.stringify(event.detail));
-   },
+        //alert(JSON.stringify(event.detail));
+        document.getElementById("txtres").value += "Login event +\n" +JSON.stringify(event.detail);
+    },
     onLogout: function () {
-        alert("Logout");
+        //alert("Logout");
+        document.getElementById("txtres").value += "\nLogout event\n";
     }
+
+
 };
 
 app.initialize();
+
+
+
